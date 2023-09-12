7db8a93e608d036af4556e0f440bd77008d7bd7c
Meow <tabbathapischke@gmail.com>
2023-09-12 12:22:41 -1000

    move extra js code to new file, create day boxes

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,14 @@ function getApi() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?zip=96786,us&appid=ef86c0944076d1643a343ec8b308ba52';
 //       var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?id='+ cityId + '&appid=ef86c0944076d1643a343ec8b308ba52';
 
-let cityId = 
 
-function getCityWeather {
-    fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-  });
-}  
+fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+    });
 }
-
-function displayWeather {
-   if(cityId === error) {
-    weatherContainerEl.textContent = 'No city found.'
-   }
-}
-
-
 getApi();
 
