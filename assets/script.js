0ea2edf850ff294bac231415ff29a933203f0cda
Meow <tabbathapischke@gmail.com>
2023-09-12 15:08:37 -1000

    add current weather, conversion

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,38 @@ fetch(requestUrl)
         console.log(data.list[0].wind.speed);
         console.log(data.list[0].main.temp);
         console.log(data.list[0].main.humidity);
+        console.log(data.list[0].dt_txt);
 
+
+
+let currentTempResponse = data.list[0].main.temp;
+let currentCity = data.city.name;
+let currentCountry = data.city.country;
+
+currentTemp.textContent = currentTempResponse;
+convertKelvinToFahrenheit(currentTempResponse);
+
+currentLocation.textContent = currentCity + ", " + currentCountry
 
     });
 
 }
 getApi();
 
+let currentTemp = document.querySelector("#current-temp");
+let currentLocation = document.querySelector("#current-location")
+
+// Source: https://codepen.io/CorbinMB/pen/OBXjMj
+function convertKelvinToFahrenheit(x) {
+    const kelvin = x;
+    // Converting kelvin to celsius by subtracting 273.
+    const celsius = kelvin - 273;
+    // Conversion to fahrenheit.
+    let fahrenheit = celsius * ( 9 / 5 ) + 32;
+    // This rounds the fahrenheit temparture.
+    fahrenheit = Math.floor(fahrenheit);
+    // Print to console.
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+    currentTemp.textContent = fahrenheit + " degrees F";
+}
