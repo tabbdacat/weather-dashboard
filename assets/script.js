4f60663fb57bfe7aa3ce0cdff2160724596a03f3
Meow <tabbathapischke@gmail.com>
2023-09-14 08:51:31 -1000

    add event listener and city input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?zip=96786,us&appid=ef86c0944076d1643a343ec8b308ba52';
+function getApi(city) {
+    // var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?zip=96786,us&appid=ef86c0944076d1643a343ec8b308ba52';
 //       var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?id='+ cityId + '&appid=ef86c0944076d1643a343ec8b308ba52';
+    // var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=honolulu&appid=ef86c0944076d1643a343ec8b308ba52';
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=ef86c0944076d1643a343ec8b308ba52';
 
 
 fetch(requestUrl)
@@ -29,7 +31,19 @@ currentLocation.textContent = currentCity + ", " + currentCountry
     });
 
 }
-getApi();
+const searchBtn = document.querySelector("#search-btn");
+let cityInput = document.querySelector("#city-input");
+
+
+searchBtn.addEventListener("click", function() {
+  console.log("click!");
+  let city = cityInput.value.trim();
+
+  console.log(city);
+
+  getApi(city);
+});
+
 
 let currentTemp = document.querySelector("#current-temp");
 let currentLocation = document.querySelector("#current-location")
@@ -48,3 +62,16 @@ function convertKelvinToFahrenheit(x) {
 
     currentTemp.textContent = fahrenheit + " degrees F";
 }
+// function getGeocodeAPI() {
+//     let requestUrlTwo = 'http://api.openweathermap.org/geo/1.0/direct?q=Wahiawa,hi,us&appid=ef86c0944076d1643a343ec8b308ba52';
+
+//     fetch(requestUrlTwo)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         console.log(data);
+//     });
+// }
+
+// getGeocodeAPI()
